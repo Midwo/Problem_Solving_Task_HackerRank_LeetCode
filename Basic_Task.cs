182feab29df35bdc0aba1d4820e0b647b4f48a2b
Michał Dwojak <michal.dwojak92@gmail.com>
2024-12-06 11:17:37 +0100

    Add: new Task: Cavity Map and Happy Ladybugs

---
+++ Basic_Task.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    internal class Basic_Task
+    {
+        public string Happy_ladybugs(string b)
+        {
+            //b = "RBY_YBR";
+            Dictionary<int, int> dictionaryString = new Dictionary<int, int>();
+            int howManyChar95 = 0;
+            int howManyOtherChar = 0;
+            int howLongString = b.Length;
+            char firstChar = b[0];
+            char nextChar = b[0];
+            int twoWrongValues = 0;
+
+            foreach (char s in b)
+            {
+                if (s != 95)
+                {
+                    howManyOtherChar++;
+                    if (dictionaryString.ContainsKey(s))
+                    {
+                        dictionaryString[s]++;
+                    }
+                    else
+                    {
+                        dictionaryString.Add(s, 1);
+                    }
+                }
+                else
+                {
+                    howManyChar95++;
+                }
+            }
+
+            if (dictionaryString.ContainsValue(1))
+            {
+                return "NO";
+            }
+            else
+            {
+                if (howManyChar95 > 0)
+                {
+                    return "YES";
+                }
+                else
+                {
+                    for (int i = 1; i < b.Length; i++)
+                    {
+                        nextChar = b[i];
+                        if (firstChar == nextChar)
+                        {
+                            firstChar = b[i];
+                            twoWrongValues = 0;
+                        }
+                        else
+                        {
+                            firstChar = b[i];
+                            twoWrongValues++;
+                            if (twoWrongValues >= 2)
+                            {
+                                return "NO";
+                            }
+                        }
+                    }
+                    if (twoWrongValues < 2)
+                    {
+                        return "YES";
+                    }
+                    else
+                    {
+                        return "NO";
+                    }
+                }
+            }
+        }
+
+        public List<string> Cavity_Map(List<string> grid)
+        {
+            //grid = ["1112", "1912", "1892", "1234"];
+            int lenghtStringInGrid = grid[0].Length;
+            int countGridMinusOne = grid.Count - 1;
+            List<string> newGrid = new List<string>();
+            newGrid = grid.ToList();
+
+            for (int i = 1; i < lenghtStringInGrid - 1; i++)
+            {
+                for (int x = 1; x < countGridMinusOne; x++)
+                {
+                    int actualRecordValue = Int32.Parse(grid[x][i].ToString());
+                    if (actualRecordValue > Int32.Parse(grid[x - 1][i].ToString()) &&
+                    actualRecordValue > Int32.Parse(grid[x + 1][i].ToString()) &&
+                    actualRecordValue > Int32.Parse(grid[x][i - 1].ToString()) &&
+                    actualRecordValue > Int32.Parse(grid[x][i + 1].ToString()))
+                    {
+                        newGrid[x] = newGrid[x].Substring(0, i) + "X" + newGrid[x].Substring(i + 1);
+                    }
+                }
+            }
+            return newGrid;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Basic_Task basic_Task = new Basic_Task();
+
+            //Happy Ladybugs
+            Console.WriteLine(basic_Task.Happy_ladybugs("RBY_YBR"));
+
+            //Cavity Map
+            List<string> grid = new List<string>();
+            grid = ["1112", "1912", "1892", "1234"];
+            grid = basic_Task.Cavity_Map(grid);
+            foreach (string item in grid)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
