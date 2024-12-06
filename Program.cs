75cc9afa3514d17af569c1f2552511af7e73df76
Michał Dwojak <michal.dwojak92@gmail.com>
2024-12-06 11:12:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProblemSolving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
