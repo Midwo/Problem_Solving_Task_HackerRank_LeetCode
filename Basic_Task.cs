bf51e41e05b0fa2a7335afb6e2ad7ed31bd88570
Michał Dwojak <michal.dwojak92@gmail.com>
2024-12-10 15:08:30 +0100

    Add: - Task: "Encryption"

--- Basic_Task.cs
+++ Basic_Task.cs
@@ -246,5 +246,47 @@
             return failRations == true ? "NO" : howManyRations.ToString();
         }
 
+        public string encryption(string s)
+        {
+            int howLong = s.Length;
+            double doubleSqrtValue = Math.Sqrt(howLong);
+            int ceilingSqrtValue = (int)Math.Ceiling(doubleSqrtValue);
+            int floorSqrtValue = (int)Math.Floor(doubleSqrtValue);
+            if (ceilingSqrtValue * floorSqrtValue < howLong)
+            {
+                floorSqrtValue = ceilingSqrtValue;
+            }
+            List<string> listEncryption = new List<string>();
+            for (int i = 0; i < floorSqrtValue; i++)
+            {
+                if (i == floorSqrtValue - 1)
+                {
+                    listEncryption.Add(s.Substring(i * ceilingSqrtValue));
+                }
+                else
+                {
+                    listEncryption.Add(s.Substring(i * ceilingSqrtValue, ceilingSqrtValue));
+                }
+            }
+            int howManyLenghtLastRow = listEncryption[floorSqrtValue - 1].Length;
+            string newOutput = string.Empty;
+            for (int i = 0; i < ceilingSqrtValue; i++)
+            {
+                for (int x = 0; x < floorSqrtValue; x++)
+                {
+                    if (i < howManyLenghtLastRow)
+                    {
+                        newOutput += listEncryption[x][i];
+                    }
+                    else if (x < floorSqrtValue - 1)
+                    {
+                        newOutput += listEncryption[x][i];
+                    }
+                }
+                newOutput += " ";
+            }
+            return newOutput.Trim();
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,54 +8,58 @@ namespace ProblemSolving
         {
             Basic_Task basic_Task = new Basic_Task();
 
-            //Apple and Orange
-            List<int> oranges = new List<int>();
-            oranges = [5, -6];
-            List<int> apples = new List<int>();
-            apples = [-2, 2, 1];
-            int b = 2;
-            int a = 3;
-            int t = 15;
-            int s = 5;
-            basic_Task.countApplesAndOranges(s, t, a, b, apples, oranges);
+            ////Apple and Orange
+            //List<int> oranges = new List<int>();
+            //oranges = [5, -6];
+            //List<int> apples = new List<int>();
+            //apples = [-2, 2, 1];
+            //int b = 2;
+            //int a = 3;
+            //int t = 15;
+            //int s = 5;
+            //basic_Task.countApplesAndOranges(s, t, a, b, apples, oranges);
 
-            //Grading Students
-            List<int> grades = new List<int>();
-            grades = [73, 67, 38, 33];
-            foreach (var item in basic_Task.gradingStudents(grades))
-            {
-                Console.WriteLine(item);
-            }
-
-            //Happy Ladybugs
-            Console.WriteLine(basic_Task.Happy_ladybugs("RBY_YBR"));
-
-            //Cavity Map
-            List<string> grid = new List<string>();
-            grid = ["1112", "1912", "1892", "1234"];
-            grid = basic_Task.Cavity_Map(grid);
-            foreach (string item in grid)
-            {
-                Console.WriteLine(item);
-            }
-
-            //Modified Kaprekar Numbers
-            Console.WriteLine(basic_Task.kaprekarNumbers(1, 100));
-
-            //ACM ICPC TEAM
-            List<string> topic = new List<string>();
-            topic = ["10101", "11100", "11010", "00101"];
-            List<int> newList = basic_Task.acmTeam(topic);
-            //foreach (var item in newList)
+            ////Grading Students
+            //List<int> grades = new List<int>();
+            //grades = [73, 67, 38, 33];
+            //foreach (var item in basic_Task.gradingStudents(grades))
             //{
             //    Console.WriteLine(item);
             //}
-            Console.WriteLine(String.Join(" ", newList.ToArray()));
 
-            //Fair Rations
-            List<int> B = new List<int>();
-            B = [1, 2];
-            Console.WriteLine(basic_Task.fairRations(B));
+            ////Happy Ladybugs
+            //Console.WriteLine(basic_Task.Happy_ladybugs("RBY_YBR"));
+
+            ////Cavity Map
+            //List<string> grid = new List<string>();
+            //grid = ["1112", "1912", "1892", "1234"];
+            //grid = basic_Task.Cavity_Map(grid);
+            //foreach (string item in grid)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            ////Modified Kaprekar Numbers
+            //Console.WriteLine(basic_Task.kaprekarNumbers(1, 100));
+
+            ////ACM ICPC TEAM
+            //List<string> topic = new List<string>();
+            //topic = ["10101", "11100", "11010", "00101"];
+            //List<int> newList = basic_Task.acmTeam(topic);
+            ////foreach (var item in newList)
+            ////{
+            ////    Console.WriteLine(item);
+            ////}
+            //Console.WriteLine(String.Join(" ", newList.ToArray()));
+
+            ////Fair Rations
+            //List<int> B = new List<int>();
+            //B = [1, 2];
+            //Console.WriteLine(basic_Task.fairRations(B));
+
+            //Encryption
+            string s = "chillout";
+            Console.WriteLine(basic_Task.encryption(s));
 
         }
     }
