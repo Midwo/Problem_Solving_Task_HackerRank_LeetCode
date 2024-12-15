e7d237fd8ad8c8ae339211e493b907d90a685dda
Michał Dwojak <michal.dwojak92@gmail.com>
2024-12-15 20:07:06 +0100

    Add: Task "Greedy Florist"

--- Basic_Task.cs
+++ Basic_Task.cs
@@ -545,5 +545,35 @@
             }
             return outputPrint;
         }
+
+        public int getMinimumCost(int k, int[] c)
+        {
+
+            int howManyFlowers = c.Count();
+            Array.Sort(c);
+            int cost = 0;
+            int multipleCost = 1;
+            int couterForMultipleCost = 0;
+            if (howManyFlowers <= k)
+            {
+                //cost = c.Sum();
+                return c.Sum();
+            }
+            else
+            {
+                do
+                {
+                    couterForMultipleCost++;
+                    if (couterForMultipleCost > k)
+                    {
+                        couterForMultipleCost = 1;
+                        multipleCost++;
+                    }
+                    cost += multipleCost * c[howManyFlowers - 1];
+                    howManyFlowers -= 1;
+                } while (howManyFlowers != 0);
+            }
+            return cost;
+        }
     }
 }
