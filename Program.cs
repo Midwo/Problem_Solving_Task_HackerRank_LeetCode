e4aab9996ab5ed09b680c26c1b08f3278cc515e9
Michał Dwojak <michal.dwojak92@gmail.com>
2024-12-29 17:54:56 +0100

    Edit: Task number 32->35.

--- Program.cs
+++ Program.cs
@@ -199,8 +199,10 @@ namespace ProblemSolving
             ////(28.) Find the Index of the First Occurrence in a String (EASY)
             //Console.WriteLine(leetCodeAll.StrStr("aaa", "aa"));
 
-            ////(32.) Search Insert Position
-            Console.WriteLine(leetCodeAll.SearchInsert([1, 3, 5, 6], 7));
+            ////(35.) Search Insert Position
+            //Console.WriteLine(leetCodeAll.SearchInsert([1, 3, 5, 6], 7));
+
+
 
             //LEETCODE INTERVIEW 150  //LEETCODE INTERVIEW 150 //LEETCODE INTERVIEW 150 //LEETCODE INTERVIEW 150 //LEETCODE INTERVIEW 150 //LEETCODE INTERVIEW 150
             //LEETCODE INTERVIEW 150 //LEETCODE INTERVIEW 150 //LEETCODE INTERVIEW 150 //LEETCODE INTERVIEW 150 //LEETCODE INTERVIEW 150 //LEETCODE INTERVIEW 150 
