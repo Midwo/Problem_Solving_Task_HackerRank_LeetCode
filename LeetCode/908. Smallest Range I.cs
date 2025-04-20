576a1a68e53d1135c42259ad1e1e105f9615145d
Michał Dwojak <michal.dwojak92@gmail.com>
2025-04-20 16:12:49 +0200

    Add: Task: LeetCode: 908. Smallest Range I

---
+++ LeetCode/908. Smallest Range I.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.LeetCode
+{
+    internal class LeetCode_908
+    {
+        ////(908.) Smallest Range I (EASY)
+        public int SmallestRangeI(int[] nums, int k)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currValue = nums[i];
+                if (currValue > max)
+                {
+                    max = currValue;
+                }
+                if(currValue < min)
+                {
+                    min = currValue;
+                }
+            }
+            int currMin = min + k;
+            int currMax = max - k;
+            int diff = currMax - currMin;
+            
+            if (diff <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return diff;
+            }
+        }
+    }
+}
--- LeetCodeAll.cs
+++ LeetCodeAll.cs
@@ -6618,8 +6618,35 @@ namespace ProblemSolving
 
             IncreasingBSTIn(root.right);
         }
+        public int SmallestRangeI(int[] nums, int k)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currValue = nums[i];
+                if (currValue > max)
+                {
+                    max = currValue;
+                }
+                if (currValue < min)
+                {
+                    min = currValue;
+                }
+            }
+            int currMin = min + k;
+            int currMax = max - k;
+            int diff = currMax - currMin;
 
-
+            if (diff <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return diff;
+            }
+        }
 
 
 
