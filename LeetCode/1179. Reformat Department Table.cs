483739654f9b8fe251998a79d99f8a219fb048a7
Michał Dwojak <michal.dwojak92@gmail.com>
2025-05-09 23:17:05 +0200

    Add: Task: LeetCode: 1179. Reformat Department Table

---
+++ LeetCode/1179. Reformat Department Table.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.LeetCode
+{
+    internal class LeetCode_1179
+    {
+
+        /////1179. Reformat Department Table (EASY)
+        string SqlQuery =
+            @"
+                    Select 
+                        id,
+                        SUM(IIF(month ='Jan', Revenue, null)) as Jan_Revenue,
+                        SUM(IIF(month ='Feb', Revenue, null)) as Feb_Revenue,
+                        SUM(IIF(month ='Mar', Revenue, null)) as Mar_Revenue,
+                        SUM(IIF(month ='Apr', Revenue, null)) as Apr_Revenue,
+                        SUM(IIF(month ='May', Revenue, null)) as May_Revenue,
+                        SUM(IIF(month ='Jun', Revenue, null)) as Jun_Revenue,
+                        SUM(IIF(month ='Jul', Revenue, null)) as Jul_Revenue,
+                        SUM(IIF(month ='Aug', Revenue, null)) as Aug_Revenue,
+                        SUM(IIF(month ='Sep', Revenue, null)) as Sep_Revenue,
+                        SUM(IIF(month ='Oct', Revenue, null)) as Oct_Revenue,
+                        SUM(IIF(month ='Nov', Revenue, null)) as Nov_Revenue,
+                        SUM(IIF(month ='Dec', Revenue, null)) as Dec_Revenue
+                    From Department 
+                    Group by id
+                ";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3297,16 +3297,35 @@ namespace ProblemSolving
             //Console.WriteLine(leetCodeAll.CountCharacters(words, chars));
 
             ////(1175.) Prime Arrangements (EASY)
-            int n = 5;
-            
-            LeetCode_1175 classLeetCode = new LeetCode_1175();
-            Console.WriteLine(classLeetCode.NumPrimeArrangements(n));
+            //int n = 5;
 
-            //OR
+            //LeetCode_1175 classLeetCode = new LeetCode_1175();
+            //Console.WriteLine(classLeetCode.NumPrimeArrangements(n));
 
-            Console.WriteLine(leetCodeAll.NumPrimeArrangements(n));
+            ////OR
 
+            //Console.WriteLine(leetCodeAll.NumPrimeArrangements(n));
 
+            /////1179. Reformat Department Table (EASY)
+            string SqlQuery =
+                @"
+                    Select 
+                        id,
+                        SUM(IIF(month ='Jan', Revenue, null)) as Jan_Revenue,
+                        SUM(IIF(month ='Feb', Revenue, null)) as Feb_Revenue,
+                        SUM(IIF(month ='Mar', Revenue, null)) as Mar_Revenue,
+                        SUM(IIF(month ='Apr', Revenue, null)) as Apr_Revenue,
+                        SUM(IIF(month ='May', Revenue, null)) as May_Revenue,
+                        SUM(IIF(month ='Jun', Revenue, null)) as Jun_Revenue,
+                        SUM(IIF(month ='Jul', Revenue, null)) as Jul_Revenue,
+                        SUM(IIF(month ='Aug', Revenue, null)) as Aug_Revenue,
+                        SUM(IIF(month ='Sep', Revenue, null)) as Sep_Revenue,
+                        SUM(IIF(month ='Oct', Revenue, null)) as Oct_Revenue,
+                        SUM(IIF(month ='Nov', Revenue, null)) as Nov_Revenue,
+                        SUM(IIF(month ='Dec', Revenue, null)) as Dec_Revenue
+                    From Department 
+                    Group by id
+                ";
 
 
 
