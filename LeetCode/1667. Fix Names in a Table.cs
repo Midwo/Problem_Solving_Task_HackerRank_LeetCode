4d275286a17f2caf236483ec0de9dc4e05362a68
Michał Dwojak <michal.dwojak92@gmail.com>
2025-05-15 22:00:34 +0200

    Add: Task: LeetCode: 1667. Fix Names in a Table

---
+++ LeetCode/1667. Fix Names in a Table.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.LeetCode
+{
+    internal class LeetCode_1667
+    {
+        ////(1667.) Fix Names in a Table (EASY)
+        string SqlQuery =
+                @"
+                    Select 
+                        user_id,
+                        Concat(UPPER(SUBSTRING(name, 1, 1)),LOWER(SUBSTRING(name, 2, LEN(name)))) as name  
+                    From Users 
+                    order by user_id     
+                ";
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3646,6 +3646,19 @@ namespace ProblemSolving
             ////OR
             //LeetCode_1661 classLeetCode = new LeetCode_1661();
 
+            ////(1667.) Fix Names in a Table (EASY)
+            string SqlQuery =
+                @"
+                    Select 
+                        user_id,
+                        Concat(UPPER(SUBSTRING(name, 1, 1)),LOWER(SUBSTRING(name, 2, LEN(name)))) as name  
+                    From Users 
+                    order by user_id     
+                ";
+
+            //OR
+            LeetCode_1667 classLeetCode = new LeetCode_1667();
+
             ////1683. Invalid Tweets (EASY)
             //string SqlQuery =
             //    @"
