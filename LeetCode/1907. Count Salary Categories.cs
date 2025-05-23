49becd0083a0b88878696cbf4bc896028c345d8d
Michał Dwojak <michal.dwojak92@gmail.com>
2025-05-23 16:27:43 +0200

    Add: Task: LeetCode: 1907. Count Salary Categories

---
+++ LeetCode/1907. Count Salary Categories.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.LeetCode
+{
+    internal class LeetCode_1907
+    {
+        ////(1907.) Count Salary Categories (MEDIUM)
+        string SqlQuery =
+            @"
+                  Select
+                      'High Salary' as category,
+                      Count(account_id) as accounts_count
+                  From 
+                  (
+                      Select 
+                          account_id
+                      From Accounts 
+                      Where income > 50000
+                  ) a 
+                  UNION ALL
+                  Select
+                      'Average Salary' as category,
+                      Count(account_id) as accounts_count
+                  From 
+                  (
+                      Select 
+                          account_id
+                      From Accounts 
+                      Where income >= 20000 and income <= 50000
+                  ) a 
+                  UNION ALL
+                  Select
+                      'Low Salary' as category,
+                      Count(account_id) as accounts_count
+                  From 
+                  (
+                      Select 
+                          account_id
+                      From Accounts 
+                      Where income < 20000
+                  ) a 
+            ";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3665,28 +3665,28 @@ namespace ProblemSolving
             //}
 
             ////(1204.) Last Person to Fit in the Bus (MEDIUM)
-            string SqlQuery =
-                @"
-                     Select top 1
-                         a.person_name
-                     FROM
-                     ( 
-                         Select
-                             person_id, 
-                             person_name,
-                             weight,
-                             turn,
-                             SUM(weight)
-                             OVER (order by turn) as SumWeight
-                         FROM Queue
-                     ) a
-                     Where SumWeight <= 1000
-                     order by a.turn desc
-                ";
+            //string SqlQuery =
+            //    @"
+            //         Select top 1
+            //             a.person_name
+            //         FROM
+            //         ( 
+            //             Select
+            //                 person_id, 
+            //                 person_name,
+            //                 weight,
+            //                 turn,
+            //                 SUM(weight)
+            //                 OVER (order by turn) as SumWeight
+            //             FROM Queue
+            //         ) a
+            //         Where SumWeight <= 1000
+            //         order by a.turn desc
+            //    ";
 
-            ////OR
+            //////OR
 
-            LeetCode_1204 classLeetCode = new LeetCode_1204();
+            //LeetCode_1204 classLeetCode = new LeetCode_1204();
 
             ////(1207.) Unique Number of Occurrences (EASY)
             //int[] arr = [-130, 21, -154, 159, -44, -126, 165, 68, -126, -126, -126, 128, -94, 165, -30, -44, -39, -94, 21, -130, 68, 68, 128, -130, -39, 181, 68, 68, 68, 139, 139, -39, 21, 21, -39, 68, 128, 131, -126, -154, -30, 165, 21, 159, 181, -39, -126, 131, -94, -44, 131, 128, 21, -44, 128, -94, 183, -94, 131, 139, -44, 128, 21, 181, -44, 131, 128, 131, 21, 68, 181, -44, -126, -130, 131, -190, 131, 181, 165, -94, 165, 165, -30, -154, 68, -39, -44, 165, -39, -126, 68, 68, -130, 68, -94, 181, -44, 131, 21, 183, -44, 21, -39, -130, -39, 131, 21, 165, 165, -126, 165, -44, -94, 68, 68, -94, -126, -126, -30, 181, 165, 68, -44, -39, -94, -126, -126, -30, 68, 181, -44, -94, -126, -44, -94, -30, 131, 165, -190, -130, -94, -94, 181, 128, 181, 181, 181, 139, -130, -94, -130, -130, 139, -130, -90, -154, 181, 165, -30, -154, 165, -190, 159, 165, 139, -126, -44, 131, -44, -190, -126, -130, -94, 128, -154, 68, -130, -130, 68, 21, -44, -30, -126, -126, 131, 159, -190, -126, 181, 139];
@@ -4061,6 +4061,46 @@ namespace ProblemSolving
 
             ////OR
             //LeetCode_1789 classLeetCode = new LeetCode_1789();
+
+            ////(1907.) Count Salary Categories (MEDIUM)
+            string SqlQuery =
+                @"
+                    Select
+                        'High Salary' as category,
+                        Count(account_id) as accounts_count
+                    From 
+                    (
+                        Select 
+                            account_id
+                        From Accounts 
+                        Where income > 50000
+                    ) a 
+                    UNION ALL
+                    Select
+                        'Average Salary' as category,
+                        Count(account_id) as accounts_count
+                    From 
+                    (
+                        Select 
+                            account_id
+                        From Accounts 
+                        Where income >= 20000 and income <= 50000
+                    ) a 
+                    UNION ALL
+                    Select
+                        'Low Salary' as category,
+                        Count(account_id) as accounts_count
+                    From 
+                    (
+                        Select 
+                            account_id
+                        From Accounts 
+                        Where income < 20000
+                    ) a 
+                ";
+
+            //OR
+            LeetCode_1907 classLeetCode = new LeetCode_1907();
 
             ////(1934.) Confirmation Rate (MEDIUM)
             //string SqlQuery =
