ed66c6b3a918bef9c4981faccb1e3544b2d5697d
Michał Dwojak <michal.dwojak92@gmail.com>
2025-05-24 11:22:14 +0200

    Add: Task: LeetCode: 1341. Movie Rating

---
+++ LeetCode/1341. Movie Rating.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.LeetCode
+{
+    internal class LeetCode_1341
+    {
+        ////(1341.) Movie Rating  (MEDIUM)
+        string SqlQuery =
+            @"
+                    Select
+                        f.results
+                    From (
+                    Select top 1
+                        c.name as results             
+                    From
+                    (
+                        Select 
+                            user_id,
+                            COUNT(user_id) as howManyTimes
+                        From MovieRating 
+                        Group by user_id 
+                    ) as a
+                    left join Users as c
+                    On c.user_id = a.user_id 
+                    Where a.howManyTimes = (Select Max(b.howManyTimes) From (Select 
+                            user_id,
+                            COUNT(user_id) as howManyTimes
+                            From MovieRating 
+                            Group by user_id ) b)
+                    order by c.name
+                    ) f
+                    
+                    UNION ALL 
+                    
+                    Select
+                        g.results
+                    From 
+                    ( 
+                        Select top 1
+                        d.title as results
+                    From
+                    (
+                        Select
+                            movie_id,
+                            AVG(rating*1.0) as averageRating
+                        From MovieRating 
+                        Where MONTH(created_at) = 2 and YEAR(created_at) = 2020
+                        Group by movie_id
+                    ) as a
+                    Left join Movies as d
+                    On d.movie_id = a.movie_id
+                    Where a.averageRating = (Select MAX(b.averageRating) FROM 
+                                    (Select
+                                        movie_id,
+                                        AVG(rating*1.0) as averageRating
+                                    From MovieRating 
+                                    Where MONTH(created_at) = 2 and YEAR(created_at) = 2020
+                                    Group by movie_id) b
+                                    ) 
+                    order by d.title
+                    ) g
+                ";
+
+        //OR
+        LeetCode_1341 classLeetCode = new LeetCode_1341();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2143,20 +2143,20 @@ namespace ProblemSolving
             //leetCodeAll.LeetCode_620();
 
             ////(626.) Exchange Seats (MEDIUM)
-            string SqlQuery =
-                @"
-                     Select
-                         id,
-                         IIF(id % 2 = 0, 
-                     LAG(student, 1, student) OVER (order by id), 
-                     LEAD(student, 1, student) OVER (order by id)
-                         ) as student 
-                     From Seat 
-                ";
+            //string SqlQuery =
+            //    @"
+            //         Select
+            //             id,
+            //             IIF(id % 2 = 0, 
+            //         LAG(student, 1, student) OVER (order by id), 
+            //         LEAD(student, 1, student) OVER (order by id)
+            //             ) as student 
+            //         From Seat 
+            //    ";
 
-            //OR
+            ////OR
 
-            LeetCode_626 classLeetCode = new LeetCode_626();
+            //LeetCode_626 classLeetCode = new LeetCode_626();
 
             ////(627.) Swap Salary (EASY)
             //LeetCode_627 classLeetCode = new LeetCode_627();
@@ -3839,6 +3839,66 @@ namespace ProblemSolving
 
             ////OR
             //LeetCode_1327 classLeetCode = new LeetCode_1327();
+
+            ////(1341.) Movie Rating  (MEDIUM)
+            string SqlQuery =
+                @"
+                    Select
+                        f.results
+                    From (
+                    Select top 1
+                        c.name as results             
+                    From
+                    (
+                        Select 
+                            user_id,
+                            COUNT(user_id) as howManyTimes
+                        From MovieRating 
+                        Group by user_id 
+                    ) as a
+                    left join Users as c
+                    On c.user_id = a.user_id 
+                    Where a.howManyTimes = (Select Max(b.howManyTimes) From (Select 
+                            user_id,
+                            COUNT(user_id) as howManyTimes
+                            From MovieRating 
+                            Group by user_id ) b)
+                    order by c.name
+                    ) f
+                    
+                    UNION ALL 
+                    
+                    Select
+                        g.results
+                    From 
+                    ( 
+                        Select top 1
+                        d.title as results
+                    From
+                    (
+                        Select
+                            movie_id,
+                            AVG(rating*1.0) as averageRating
+                        From MovieRating 
+                        Where MONTH(created_at) = 2 and YEAR(created_at) = 2020
+                        Group by movie_id
+                    ) as a
+                    Left join Movies as d
+                    On d.movie_id = a.movie_id
+                    Where a.averageRating = (Select MAX(b.averageRating) FROM 
+                                    (Select
+                                        movie_id,
+                                        AVG(rating*1.0) as averageRating
+                                    From MovieRating 
+                                    Where MONTH(created_at) = 2 and YEAR(created_at) = 2020
+                                    Group by movie_id) b
+                                    ) 
+                    order by d.title
+                    ) g
+                ";
+
+            //OR
+            LeetCode_1341 classLeetCode = new LeetCode_1341();
 
             ////(1378.) Replace Employee ID With The Unique Identifier (EASY)
             //string SqlQuery =
