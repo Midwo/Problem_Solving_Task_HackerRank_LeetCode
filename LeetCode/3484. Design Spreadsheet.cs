6e76f19b0517e409e6deb166af59866003bd5ea1
Michał Dwojak <michal.dwojak92@gmail.com>
2025-09-19 11:52:40 +0200

    Add: Task: LeetCode: 3484. Design Spreadsheet

---
+++ LeetCode/3484. Design Spreadsheet.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.LeetCode
+{
+    internal class LeetCode_3484
+    {
+        ////(3484.) Design Spreadsheet (MEDIUM)
+        public class Spreadsheet
+        {
+            //// 28ms, Beats: 80.00%
+            
+            private int[][] _tableExcel;
+            public Spreadsheet(int rows)
+            {
+                _tableExcel = new int [26][];
+                for (int i = 0; i < 26; i++)
+                {
+                    _tableExcel[i] = new int[rows];
+                }
+            }
+
+            public void SetCell(string cell, int value)
+            {
+                _tableExcel[(cell[0] - 'A')][int.Parse(cell[1..cell.Length])-1] = value;
+            }
+
+            public void ResetCell(string cell)
+            {
+                _tableExcel[(cell[0] - 'A')][int.Parse(cell[1..cell.Length]) - 1] = 0;
+            }
+
+            public int GetValue(string formula)
+            {
+                string[] tempString = formula.Split('+');
+                int value1 = 0;
+                if (tempString[0][1] >= 'A' && tempString[0][1] <= 'Z')
+                {
+                    value1 = _tableExcel[tempString[0][1] - 'A'][int.Parse(tempString[0][2..tempString[0].Length])-1];
+                }
+                else
+                {
+                    value1 = int.Parse(tempString[0][1..tempString[0].Length]);
+                }
+                if (tempString[1][0] >= 'A' && tempString[1][0] <= 'Z')
+                {
+                    return value1 + (_tableExcel[tempString[1][0] - 'A'][int.Parse(tempString[1][1..tempString[1].Length]) - 1]);
+                }
+                else
+                {
+                    return value1 + int.Parse(tempString[1]);
+                }
+            }
+        }
+
+    }
+}
--- LeetCodeAll.cs
+++ LeetCodeAll.cs
@@ -16146,8 +16146,52 @@ namespace ProblemSolving
                 currPath.RemoveAt(currPath.Count() - 1);
             }
         }
+        public class SpreadsheetTask3484
+        {
+            //// 28ms, Beats: 80.00%
 
+            private int[][] _tableExcel;
+            public SpreadsheetTask3484(int rows)
+            {
+                _tableExcel = new int[26][];
+                for (int i = 0; i < 26; i++)
+                {
+                    _tableExcel[i] = new int[rows];
+                }
+            }
 
+            public void SetCell(string cell, int value)
+            {
+                _tableExcel[(cell[0] - 'A')][int.Parse(cell[1..cell.Length]) - 1] = value;
+            }
+
+            public void ResetCell(string cell)
+            {
+                _tableExcel[(cell[0] - 'A')][int.Parse(cell[1..cell.Length]) - 1] = 0;
+            }
+
+            public int GetValue(string formula)
+            {
+                string[] tempString = formula.Split('+');
+                int value1 = 0;
+                if (tempString[0][1] >= 'A' && tempString[0][1] <= 'Z')
+                {
+                    value1 = _tableExcel[tempString[0][1] - 'A'][int.Parse(tempString[0][2..tempString[0].Length]) - 1];
+                }
+                else
+                {
+                    value1 = int.Parse(tempString[0][1..tempString[0].Length]);
+                }
+                if (tempString[1][0] >= 'A' && tempString[1][0] <= 'Z')
+                {
+                    return value1 + (_tableExcel[tempString[1][0] - 'A'][int.Parse(tempString[1][1..tempString[1].Length]) - 1]);
+                }
+                else
+                {
+                    return value1 + int.Parse(tempString[1]);
+                }
+            }
+        }
 
 
 
