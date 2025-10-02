10701e6b41c96f64a1d2030fa8cdf9cc44355c8e
Michał Dwojak <michal.dwojak92@gmail.com>
2025-10-02 09:53:45 +0200

    Add: Task: LeetCode: 2918. Minimum Equal Sum of Two Arrays After Replacing Zeros

--- LeetCodeAll.cs
+++ LeetCodeAll.cs
@@ -17189,7 +17189,73 @@ namespace ProblemSolving
 
             return false;
         }
+        public long MinSum(int[] nums1, int[] nums2)
+        {
+            //85ms, Beasts: 93.75%
+            long sumNums1 = 0;
+            long sumNums2 = 0;
+            int countZeroNums1 = 0;
+            int countZeroNums2 = 0;
 
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                if (nums1[i] > 0)
+                {
+                    sumNums1 += nums1[i];
+                }
+                else
+                {
+                    countZeroNums1++;
+                }
+            }
+
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                if (nums2[i] > 0)
+                {
+                    sumNums2 += nums2[i];
+                }
+                else
+                {
+                    countZeroNums2++;
+                }
+            }
+
+            long minSumNums1 = sumNums1 + countZeroNums1;
+            long minSumNums2 = sumNums2 + countZeroNums2;
+
+
+            if (minSumNums1 >= minSumNums2)
+            {
+                if (countZeroNums2 > 0)
+                {
+                    return minSumNums1;
+                }
+                else if (minSumNums1 == minSumNums2)
+                {
+                    return minSumNums1;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                if (countZeroNums1 > 0)
+                {
+                    return minSumNums2;
+                }
+                else if (minSumNums1 == minSumNums2)
+                {
+                    return minSumNums2;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
 
 
 
