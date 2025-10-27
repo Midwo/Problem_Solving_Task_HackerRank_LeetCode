877bccb614e522fb7c681bdef8a7ce5fea51fce6
Michał Dwojak <michal.dwojak92@gmail.com>
2025-10-27 19:47:31 +0100

    Add: Task: LeetCode: 2278. Percentage of Letter in String

---
+++ LeetCode/2278. Percentage of Letter in String.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.LeetCode
+{
+    internal class LeetCode_2278
+    {
+        ////(2278.) Percentage of Letter in String (EASY)
+        public int PercentageLetter(string s, char letter)
+        {
+            ////0ms, Beats: 100.00%
+            
+            int count = 0;
+            int length = s.Length;
+
+            for (int i = 0; i < length; i++) 
+            { 
+                if (s[i] == letter)
+                {
+                    count++;
+                }
+            }
+
+            return count*100 / length;
+        }
+    }
+}
--- LeetCodeAll.cs
+++ LeetCodeAll.cs
@@ -19184,6 +19184,25 @@ namespace ProblemSolving
 
             return result;
         }
+        public int PercentageLetter(string s, char letter)
+        {
+            ////0ms, Beats: 100.00%
+
+            int count = 0;
+            int length = s.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (s[i] == letter)
+                {
+                    count++;
+                }
+            }
+
+            return count * 100 / length;
+        }
+
+
 
 
 
