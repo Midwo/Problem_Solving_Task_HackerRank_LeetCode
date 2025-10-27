31d8d05e9bc574783aea4a7054e9c47e91b21d59
Michał Dwojak <michal.dwojak92@gmail.com>
2025-10-27 19:39:27 +0100

    Add: Task: LeetCode: 2903. Find Indices With Index and Value Difference I

---
+++ LeetCode/2903. Find Indices With Index and Value Difference I.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.LeetCode
+{
+    internal class LeetCode_2903
+    {
+        ////(2903.) Find Indices With Index and Value Difference I (EASY)
+        public int[] FindIndices(int[] nums, int indexDifference, int valueDifference)
+        {
+            ////0ms, Beats: 100.00%
+            
+            int[] result = new int[2] { -1, -1 };
+            int length = nums.Length;
+
+            for (int i = 0; i < length; i++) 
+            { 
+                int tempValue = nums[i];
+                for (int j = i+indexDifference; j < length; j++)
+                {
+                    if (Math.Abs(tempValue - nums[j]) >= valueDifference)
+                    {
+                        return new int[2] {i, j};
+                    }
+                }           
+            }
+                
+            return result;
+        }
+    }
+}
--- LeetCodeAll.cs
+++ LeetCodeAll.cs
@@ -19163,8 +19163,27 @@ namespace ProblemSolving
             }
             return ' ';
         }
+        public int[] FindIndices(int[] nums, int indexDifference, int valueDifference)
+        {
+            ////0ms, Beats: 100.00%
 
+            int[] result = new int[2] { -1, -1 };
+            int length = nums.Length;
 
+            for (int i = 0; i < length; i++)
+            {
+                int tempValue = nums[i];
+                for (int j = i + indexDifference; j < length; j++)
+                {
+                    if (Math.Abs(tempValue - nums[j]) >= valueDifference)
+                    {
+                        return new int[2] { i, j };
+                    }
+                }
+            }
+
+            return result;
+        }
 
 
 
