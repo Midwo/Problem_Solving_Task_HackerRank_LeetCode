f1d7426e3b03f56f84a19d67f5d733d5eff7b90d
Michał Dwojak <michal.dwojak92@gmail.com>
2026-02-21 12:23:14 +0100

    Add: Task: LeetCode: 2913. Subarrays Distinct Element Sum of Squares I

---
+++ LeetCode/2913. Subarrays Distinct Element Sum of Squares I.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.LeetCode
+{
+    internal class LeetCode_2913
+    {
+        ////(2913.) Subarrays Distinct Element Sum of Squares I (EASY)
+        public int SumCounts(IList<int> nums)
+        {
+            int lenght = nums.Count;
+            int result = 0;
+            for (int indexStart = 0; indexStart < lenght; indexStart++)
+            {
+                HashSet<int> set = new HashSet<int>();
+                for (int currIndex = indexStart; currIndex < lenght; currIndex++)
+                {
+                    set.Add(nums[currIndex]);
+                    result += (int)Math.Pow(set.Count, 2);
+                }
+            }
+            return result; 
+        }
+    }
+}
--- LeetCodeAll.cs
+++ LeetCodeAll.cs
@@ -26699,8 +26699,21 @@ namespace ProblemSolving
 
             return howManyNumbers;
         }
-
-
+        public int SumCounts(IList<int> nums)
+        {
+            int lenght = nums.Count;
+            int result = 0;
+            for (int indexStart = 0; indexStart < lenght; indexStart++)
+            {
+                HashSet<int> set = new HashSet<int>();
+                for (int currIndex = indexStart; currIndex < lenght; currIndex++)
+                {
+                    set.Add(nums[currIndex]);
+                    result += (int)Math.Pow(set.Count, 2);
+                }
+            }
+            return result;
+        }
 
 
 
