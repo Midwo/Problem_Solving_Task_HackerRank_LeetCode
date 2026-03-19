aec6b9b010a44bf1da31e7fbd727d2f49a9b1cf4
Michał Dwojak <michal.dwojak92@gmail.com>
2026-03-19 11:08:56 +0100

    Add: Task: LeetCode: 1817. Finding the Users Active Minutes

---
+++ LeetCode/1817. Finding the Users Active Minutes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.LeetCode
+{
+    internal class LeetCode_1817
+    {
+        ////(1817.) Finding the Users Active Minutes (MEDIUM)
+        public int[] FindingUsersActiveMinutes(int[][] logs, int k)
+        {
+            int[] resultTable = new int[k];
+            Dictionary<int, HashSet<int>> usersAndDistinctActives = new Dictionary<int, HashSet<int>>();
+            int length = logs.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                int currID = logs[i][0];
+                int currTime = logs[i][1];
+
+                if (usersAndDistinctActives.ContainsKey(currID)) //&& usersAndDistinctActives[currID].Count < k)
+                {
+                    if (usersAndDistinctActives[currID].Count < k)
+                    {
+                        usersAndDistinctActives[currID].Add(currTime);
+                    }
+                }
+                else
+                {
+                    usersAndDistinctActives.Add(currID, new HashSet<int>() { currTime });
+                }
+            }
+
+            foreach (var item in usersAndDistinctActives)
+            {
+                if (item.Value.Count < k)
+                {
+                    resultTable[item.Value.Count-1]++;
+                }
+            }
+
+            return resultTable;
+        }
+    }
+}
--- LeetCodeAll.cs
+++ LeetCodeAll.cs
@@ -28550,6 +28550,49 @@ namespace ProblemSolving
 
             return countSubmatices;
         }
+        public int[] FindingUsersActiveMinutes(int[][] logs, int k)
+        {
+            int[] resultTable = new int[k];
+            Dictionary<int, HashSet<int>> usersAndDistinctActives = new Dictionary<int, HashSet<int>>();
+            int length = logs.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                int currID = logs[i][0];
+                int currTime = logs[i][1];
+
+                if (usersAndDistinctActives.ContainsKey(currID)) //&& usersAndDistinctActives[currID].Count < k)
+                {
+                    if (usersAndDistinctActives[currID].Count < k)
+                    {
+                        usersAndDistinctActives[currID].Add(currTime);
+                    }
+                }
+                else
+                {
+                    usersAndDistinctActives.Add(currID, new HashSet<int>() { currTime });
+                }
+            }
+
+            foreach (var item in usersAndDistinctActives)
+            {
+                if (item.Value.Count < k)
+                {
+                    resultTable[item.Value.Count - 1]++;
+                }
+            }
+
+            return resultTable;
+        }
+
+
+
+
+
+
+
+
+
 
 
 
